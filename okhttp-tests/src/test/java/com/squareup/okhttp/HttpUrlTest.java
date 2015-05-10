a344bb9f1ca8db66fb336192f9d5bec7645f5e07
jwilson <jwilson@squareup.com>
2015-05-09 21:24:26 -0400

    Use lists to model paths and queries.
    This is actually a little trickier than strings, partly because
    we now need code to go in both directions, and partly because
    paths that end with '/' are an important but awkward special
    case.
    
    The goal is that this will make accessing query parameters much
    easier.

--- okhttp-tests/src/test/java/com/squareup/okhttp/HttpUrlTest.java
+++ okhttp-tests/src/test/java/com/squareup/okhttp/HttpUrlTest.java
@@ -394,6 +394,21 @@ public final class HttpUrlTest {
     assertEquals(HttpUrl.parse("http://host/a/b/"), base.resolve("%2e"));
   }
 
+  @Test public void relativePathWithTrailingSlash() throws Exception {
+    HttpUrl base = HttpUrl.parse("http://host/a/b/c/");
+    assertEquals(HttpUrl.parse("http://host/a/b/"), base.resolve(".."));
+    assertEquals(HttpUrl.parse("http://host/a/b/"), base.resolve("../"));
+    assertEquals(HttpUrl.parse("http://host/a/"), base.resolve("../.."));
+    assertEquals(HttpUrl.parse("http://host/a/"), base.resolve("../../"));
+    assertEquals(HttpUrl.parse("http://host/"), base.resolve("../../.."));
+    assertEquals(HttpUrl.parse("http://host/"), base.resolve("../../../"));
+    assertEquals(HttpUrl.parse("http://host/"), base.resolve("../../../.."));
+    assertEquals(HttpUrl.parse("http://host/"), base.resolve("../../../../"));
+    assertEquals(HttpUrl.parse("http://host/a"), base.resolve("../../../../a"));
+    assertEquals(HttpUrl.parse("http://host/"), base.resolve("../../../../a/.."));
+    assertEquals(HttpUrl.parse("http://host/a/"), base.resolve("../../../../a/b/.."));
+  }
+
   @Test public void pathWithBackslash() throws Exception {
     HttpUrl base = HttpUrl.parse("http://host/a/b/c");
     assertEquals(HttpUrl.parse("http://host/a/b/d/e/f"), base.resolve("d\\e\\f"));
@@ -414,7 +429,7 @@ public final class HttpUrlTest {
 
   @Test public void decodePassword() {
     assertEquals("password", HttpUrl.parse("http://user:password@host/").decodePassword());
-    assertEquals(null, HttpUrl.parse("http://user:@host/").decodePassword());
+    assertEquals("", HttpUrl.parse("http://user:@host/").decodePassword());
     assertEquals("\uD83C\uDF69", HttpUrl.parse("http://user:%F0%9F%8D%A9@host/").decodePassword());
   }
 
@@ -478,7 +493,7 @@ public final class HttpUrlTest {
     assertEquals("http://host/", url.toString());
     assertEquals("http", url.scheme());
     assertEquals("", url.username());
-    assertEquals(null, url.password());
+    assertEquals("", url.password());
     assertEquals("host", url.host());
     assertEquals(80, url.port());
     assertEquals("/", url.path());
